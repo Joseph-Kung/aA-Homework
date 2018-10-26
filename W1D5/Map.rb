e0cea1a6b454bfd61b663494b6ee40d63f02993f
Joseph-Kung <joseph.kung@hotmail.com>
2018-10-25 21:51:42 -0700

    Create Map#set method

--- W1D5/Map.rb
+++ W1D5/Map.rb
@@ -2,6 +2,18 @@ class Map
   attr_reader :my_map
 
   def initialize
-    @my_map
+    @my_map = []
+  end
+
+  def set(key, value)
+    i = 0
+
+    while i < my_map.length
+      if my_map[i].first == key
+        my_map.delete_at(i)
+      end
+      i += 1
+    end
+    my_map << [key , value]
   end
 end
