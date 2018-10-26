96ff8a9d86fb059f9b8fb4b283566dd0eeb8a830
Joseph-Kung <joseph.kung@hotmail.com>
2018-10-25 21:30:51 -0700

    Add #enqueue #dequeue and #peek features for Queue class

--- W1D5/queue.rb
+++ W1D5/queue.rb
@@ -1,8 +1,19 @@
 class Queue
-  attr_reader :question
+  attr_reader :que
 
   def initialize
     @que = []
   end
 
+  def enqueue(el)
+    que.unshift(el)
+  end
+
+  def dequeue
+    que.pop
+  end
+
+  def peek
+    que.last
+  end
 end
