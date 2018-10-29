e929b36f7ac585be20007c74bf159e55b3b94197
Joseph-Kung <joseph.kung@hotmail.com>
2018-10-28 18:26:25 -0700

    Add #require_sequence method and overall refinements to game

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -10,6 +10,8 @@ class Simon
   end
 
   def play
+    puts 'Get ready! The game is about to start!'
+    sleep(1)
     take_turn until self.game_over == true
     game_over_message
     reset_game
@@ -25,14 +27,33 @@ class Simon
       round_success_message
       self.sequence_length += 1
     end
+    sleep(2)
   end
 
   def show_sequence
     add_random_color
+    self.seq.each do |color|
+      puts "#{color}"
+      sleep(2)
+      system('clear')
+    end
   end
 
   def require_sequence
+    answer = []
+    i = 0
+    alive = true
 
+    until alive == false || self.seq == answer
+      puts 'Please enter the correct color'
+      answer << gets.chomp.downcase
+      if self.seq[i] != answer[i]
+        @game_over = true
+        alive = false
+      else
+        i += 1
+      end
+    end
   end
 
   def add_random_color
@@ -40,14 +61,20 @@ class Simon
   end
 
   def round_success_message
-
+    puts 'Good Job! On to the next round.'
+    sleep(1)
+    system('clear')
   end
 
   def game_over_message
-
+    puts 'Wrong answer... Game over!'
   end
 
   def reset_game
-
+    self.sequence_length = 1
+    self.game_over = false
+    self.seq = []
   end
 end
+
+Simon.new.play
