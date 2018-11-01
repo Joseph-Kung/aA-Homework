5e3808ed92b94f8bd649968df1466ca06e528f97
Joseph-Kung <joseph.kung@hotmail.com>
2018-10-31 21:10:39 -0700

    Write merge_sort and dominant_octopus to demonstrate O(nlogn)

--- W2D4/big_octopus.rb
+++ W2D4/big_octopus.rb
@@ -17,3 +17,35 @@ def sluggish_octopus
   end
   longest
 end
+
+#O(n log n)
+def dominant_octopus
+  prc = Proc.new { |x,y| y.length <=> x.length}
+  FISH.merge_sort(&prc).first
+end
+
+class Array
+  def merge_sort(&prc)
+    prc ||= Proc.new {|x,y| x <=> y}
+    return self if self.length <= 1
+
+    half = self.length / 2
+    left = self.take(half).merge_sort(&prc)
+    right = self.drop(half).merge_sort(&prc)
+
+    Array.merge(left, right, &prc)
+  end
+
+  def self.merge(left, right, &prc)
+    result = []
+
+    until left.empty? || right.empty?
+      if prc.call(left.first, right.first) == 1
+        result << right.shift
+      else
+        result << left.shift
+      end
+    end
+    result + left + right
+  end
+end
