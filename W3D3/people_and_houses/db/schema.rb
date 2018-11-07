9dd0d67026b467361a0549349ffc775474d15fb3
Joseph-Kung <joseph.kung@hotmail.com>
2018-11-06 20:28:26 -0800

    Create models for House and Person

--- W3D3/people_and_houses/db/schema.rb
+++ W3D3/people_and_houses/db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_041653) do
+ActiveRecord::Schema.define(version: 2018_11_07_042553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "house", force: :cascade do |t|
+    t.string "address", null: false
+  end
+
+  create_table "houses", force: :cascade do |t|
     t.string "address", null: false
   end
 
