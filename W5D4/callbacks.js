6dbf85c77c4fbca1b46845a191b9744873353c1d
Joseph-Kung <joseph.kung@hotmail.com>
2018-11-22 19:45:15 -0800

    Complete simple timeout and timeout plus closure

---
+++ W5D4/callbacks.js
@@ -0,0 +1,11 @@
+// simple timeout
+
+// window.setTimeout(function () {
+//   alert('HAMMER TIME!');
+// }, 5000);
+
+function hammerTime(time) {
+  window.setTimeout(function() {
+    alert(`${time} is hammer time!`)
+  }, time)
+}
