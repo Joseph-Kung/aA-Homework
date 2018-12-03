a7f3f2a2a19d2f81b01632c73d1068562524cf23
Joseph-Kung <joseph.kung@hotmail.com>
2018-12-02 20:50:46 -0800

    Complete job-listings homework

---
+++ W7D1/job-listings/frontend/actions.js
@@ -0,0 +1,12 @@
+const selectLocation = function (city, jobs) {
+    return {
+        type: 'SWITCH_LOCATION',
+        city,
+        jobs,
+    };
+};
+
+window.selectLocation = selectLocation;
+
+
+export default selectLocation;
