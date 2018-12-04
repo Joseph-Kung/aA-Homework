5b7c54592e7cf88f1fed79af49332f72dd57ab8d
Joseph-Kung <joseph.kung@hotmail.com>
2018-12-04 13:18:25 -0800

    Complete and test reducer, action, and store

--- W7D3/gify-search/giphy_search.jsx
+++ W7D3/gify-search/giphy_search.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store';
+import {configureStore} from './store/store';
 import Root from './components/root';
+import { fetchSearchGiphys } from './util/api_util.js';
+import { receiveSearchGiphys } from './actions/giphy_actions'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
+    const root = document.getElementById('root');
+    // ReactDOM.render(<Root />, root);
+})
