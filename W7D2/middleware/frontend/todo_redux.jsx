fd9f5b47fb8dab6789f2e9e17da9327e951557a5
Joseph-Kung <joseph.kung@hotmail.com>
2018-12-03 20:05:48 -0800

    Complete phase I and II

--- W7D2/middleware/frontend/todo_redux.jsx
+++ W7D2/middleware/frontend/todo_redux.jsx
@@ -9,6 +9,33 @@ document.addEventListener('DOMContentLoaded', () => {
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
 
+  // store.dispatch = addLoggingToDispatch(store);
+  store = applyMiddlewares(store, addLoggingToDispatch);
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+// const addLoggingToDispatch = (store) => {
+//   const dpatch = store.dispatch;
+//   return (action) => {
+//     console.log(store.getState());
+//     console.log(action);
+//     dpatch(action);
+//     console.log(store.getState());
+//   }
+// }
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
+
+const applyMiddleWares(store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch })
+};
